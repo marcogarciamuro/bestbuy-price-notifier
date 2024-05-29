2c9003e5aecbd29d3da9b25637beb102ca112cad
Marco Garcia <marcogarciamuro20@gmail.com>
2024-05-28 23:14:58 -0700

    can track purchased products and filter based on price match eligibility

--- server/server.js
+++ server/server.js
@@ -5,11 +5,7 @@ const app = express();
 const port = 3001;
 
 const { initializeApp, cert } = require("firebase-admin/app");
-const {
-  getFirestore,
-  Timestamp,
-  FieldValue,
-} = require("firebase-admin/firestore");
+const { getFirestore } = require("firebase-admin/firestore");
 
 const serviceAccount = require("./bestbuy-price-notifier-firebase-adminsdk-1buz2-8587967664.json");
 
@@ -19,14 +15,107 @@ initializeApp({
 
 const db = getFirestore();
 
+class Product {
+  constructor(
+    sku,
+    url,
+    name,
+    pricePaid,
+    currentPrice,
+    purchaseDate,
+    daysLeftToPriceMatch
+  ) {
+    this.sku = sku;
+    this.url = url;
+    this.name = name;
+    this.pricePaid = pricePaid;
+    this.currentPrice = currentPrice;
+    this.purchaseDate = purchaseDate;
+    this.daysLeftToPriceMatch = daysLeftToPriceMatch;
+  }
+}
+
 app.use(cors());
 
 app.get("/getData", async (req, res) => {
-  console.log("AFTER");
-  const productURL = req.query.productURL;
-  const userID = req.query.userID;
-  const productSKU = req.query.link.slice(-7);
-  const apiEndPoint = `https://www.bestbuy.com/api/3.0/priceBlocks?skus=${productSKU}`;
+  const productUrl = req.query.productUrl;
+  const userId = req.query.userId;
+  const pricePaid = req.query.pricePaid;
+  const productSku = req.query.productUrl.slice(-7);
+  const purchaseDate = req.query.purchaseDate;
+  console.log(req.query);
+  const apiEndpoint = `https://www.bestbuy.com/priceview/query/sku?_lazyHydrate=true&buyingOptionsOnly=true&context=PDP-PostCard-BuyingOptions&destinationZipCode=95407&deviceClass=l&effectivePlanPaidMemberType=NULL&includes=buyNowPayLater%2CdealExpirationTimer%2Coffers%2Cprice%2CpriceBadge%2CskuDataAnalytics%2CtotalTechPrice%2CtotalTechSavings%2CtradeInMessaging%2CupgradePlusCitizenOption&layout=large&planPaidMember=false&skuId=${productSku}&viewType=price&vt=1b94360d-c1a0-11ee-a37d-06049a89920b`;
+  try {
+    console.log("Visiting: ", productUrl);
+    const productInfo = await fetchData(apiEndpoint, productUrl);
+    const regularPrice = productInfo.skuDataAnalytics.regularPrice;
+    const curPrice = productInfo.skuDataAnalytics.customerPrice;
+    const memberPrice = productInfo.totalTechPrice;
+    const lowestPrice = memberPrice;
+    const onSale = productInfo.skuDataAnalytics == "savings";
+    const saleEndDate = productInfo.showDealExpirationTimer
+      ? productInfo.dealExpirationTimeStamp
+      : null;
+
+    const productName = await getProductName(productSku);
+    const daysLeftToPriceMatch = getDaysLeftToPriceMatch(userId, purchaseDate);
+    console.log("purchase date", purchaseDate);
+    console.log(daysLeftToPriceMatch);
+
+    const product = new Product(
+      productSku,
+      productUrl,
+      productName,
+      pricePaid,
+      curPrice,
+      purchaseDate,
+      daysLeftToPriceMatch
+    );
+    console.log("sending:", product);
+
+    // check if product isn't already in user's account
+    const alreadyTracked = await productIsAlreadyTracked(userId, productSku);
+    console.log(alreadyTracked);
+    if (!alreadyTracked) {
+      console.log("here");
+      await addProductToUserAccount(userId, product);
+    } else {
+      console.log("Product is already in user's account");
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+function getDaysLeftToPriceMatch(userId, purchaseDateStr) {
+  const returnPeriodInDays = getUserHasMembership(userId) ? 60 : 15;
+  const purchaseDate = new Date(purchaseDateStr);
+  var lastDayToPriceMatch = new Date(purchaseDate.valueOf());
+  lastDayToPriceMatch.setDate(
+    lastDayToPriceMatch.getDate() + returnPeriodInDays
+  );
+  const today = new Date();
+  const daysLeft = Math.round(
+    (lastDayToPriceMatch - today) / (1000 * 60 * 60 * 24)
+  );
+  return daysLeft < 0 ? 0 : daysLeft;
+}
+
+async function getUserHasMembership(userId) {
+  try {
+    const userDocRef = doc(db, "users", userId);
+    const userDocSnap = await getProductName(userDocRef);
+    if (userDocSnap.exists()) {
+      return userDocSnap.data().hasMembership;
+    }
+  } catch (error) {
+    console.log("Error getting user membership status");
+    return false;
+  }
+}
+
+async function fetchData(apiEndpoint, productUrl) {
   const headers = {
     authority: "www.bestbuy.com",
     pragma: "no-cache",
@@ -37,43 +126,59 @@ app.get("/getData", async (req, res) => {
     "sec-fetch-Dest": "empty",
     "sec-fetch-Mode": "cors",
     "sec-fetch-Site": "same-origin",
-    referer: productURL,
+    referer: productUrl,
     "accept-language": "en-US,en;q=0.9",
   };
   const axiosConfig = {
     headers: headers,
   };
+
   try {
-    console.log("Visiting: ", productURL);
-    const response = await axios.get(apiEndPoint, axiosConfig);
-    const productPageData = response.data;
-    const productName = productPageData[0].sku.names.short;
-    console.log(productPageData[0].sku);
-    // console.log(productPageData[0].sku.price.currentPrice);
-
-    await addProductToUserAccount(userID, productSKU, productName);
+    const response = await axios.get(apiEndpoint, axiosConfig);
+    const productInfo = response.data;
+    return productInfo;
   } catch (error) {
-    console.error("Error:", error.message);
-    res.status(500).send("Internal Server Error");
+    return null;
   }
-});
+}
 
-async function addProductToUserAccount(userID, productSKU, productName) {
-  console.log("ADDING PRODUCT WITH SKU: ", productSKU, "to user account");
-  // const snapshot = await db.collection("users").get();
-  // snapshot.forEach((doc) => {
-  //   console.log(doc.id, "=>", doc.data());
-  // });
+async function getProductName(productSku) {
+  const apiEndpoint = `https://www.bestbuy.com/api/3.0/priceBlocks?skus=${productSku}`;
+  const productInfo = await fetchData(apiEndpoint);
+  return productInfo[0].sku.names.short;
+}
 
+async function productIsAlreadyTracked(userId, productSku) {
+  const trackedProductsRef = db
+    .collection("users")
+    .doc(userId)
+    .collection("trackedProducts");
+  const docRef = trackedProductsRef.doc(productSku);
+  try {
+    const snapshot = await docRef.get();
+    return snapshot.exists;
+  } catch (error) {
+    console.error("Error checking document existence:".error);
+    return false;
+  }
+}
+
+async function addProductToUserAccount(userId, product) {
+  console.log("Adding product to user's account");
+  console.log(product);
   const docRef = db
     .collection("users")
-    .doc(userID)
+    .doc(userId)
     .collection("trackedProducts")
-    .doc(productSKU);
+    .doc(product.sku);
   await docRef.set({
-    name: productName,
-    amountPaid: 1299,
-    dateOfPurchase: "Today",
+    sku: product.sku,
+    productUrl: product.url,
+    name: product.name,
+    pricePaid: product.pricePaid,
+    currentPrice: product.currentPrice,
+    purchaseDate: product.purchaseDate,
+    daysLeftToPriceMatch: product.daysLeftToPriceMatch,
   });
 }
 
